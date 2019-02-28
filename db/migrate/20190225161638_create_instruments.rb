7eef99e92ada476d7a95a3c552e76b348fa34c15
Mark Evans <me@markdavidevans.com>
2019-02-28 18:10:44 +0000

    Migration change

--- db/migrate/20190225161638_create_instruments.rb
+++ db/migrate/20190225161638_create_instruments.rb
@@ -1,7 +1,6 @@
 class CreateInstruments < ActiveRecord::Migration[5.2]
   def change
     create_table :instruments do |t|
-      t.string :name
       t.string :category
       t.string :condition
       t.string :location
