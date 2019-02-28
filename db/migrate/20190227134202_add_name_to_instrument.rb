799c7d91c3b21269a15ac1b9c42507bb21b32165
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2019-02-28 10:24:20 +0000

    schema fixed

--- db/migrate/20190227134202_add_name_to_instrument.rb
+++
@@ -1,4 +0,0 @@
-class AddNameToInstrument < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20190227135000_add_details_to_instrument.rb
+++
@@ -1,5 +0,0 @@
-class AddDetailsToInstrument < ActiveRecord::Migration[5.2]
-  def change
-    add_column :instruments, :name, :string
-  end
-end
